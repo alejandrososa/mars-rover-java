320befe72870a0f0bad9df366a05b3f3bb4113b6
Alejandro Sosa <alesjohnson@hotmail.com>
2024-12-18 19:13:55 +0100

    First commit

---
+++ src/main/java/com/asosa/marsrover/MarsRoverJavaApplication.java
@@ -0,0 +1,13 @@
+package com.asosa.marsrover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asosa/marsrover/MarsRoverJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asosa.marsrover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
